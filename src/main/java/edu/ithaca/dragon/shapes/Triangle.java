c2260849e576545d1915bc469b364591f08cfd50
brookewil <bwilson4@ithaca.edu>
2024-09-05 01:39:41 -0400

    Triangle class setup

---
+++ src/main/java/edu/ithaca/dragon/shapes/Triangle.java
@@ -0,0 +1,14 @@
+package edu.ithaca.dragon.shapes;
+
+public class Triangle {
+    double sideLength1;
+    double sideLength2;
+    double sideLength3;
+
+public Triangle(double side1, double side2, double side3){
+    double sideLength1 = side1;
+    double sideLength2 = side2;
+    double sideLength3 = side3;
+}
+
+}
