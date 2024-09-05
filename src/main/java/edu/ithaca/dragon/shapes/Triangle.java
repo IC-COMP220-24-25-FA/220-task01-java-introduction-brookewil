b9e2b78013820dbcb3884acd314dba5886f0a00e
brookewil <bwilson4@ithaca.edu>
2024-09-05 01:42:19 -0400

    triangle skeleton

--- src/main/java/edu/ithaca/dragon/shapes/Triangle.java
+++ src/main/java/edu/ithaca/dragon/shapes/Triangle.java
@@ -5,10 +5,21 @@ public class Triangle {
     double sideLength2;
     double sideLength3;
 
-public Triangle(double side1, double side2, double side3){
-    double sideLength1 = side1;
-    double sideLength2 = side2;
-    double sideLength3 = side3;
-}
+    public Triangle(double side1, double side2, double side3){
+        double sideLength1 = side1;
+        double sideLength2 = side2;
+        double sideLength3 = side3;
+    }
 
+    public double calcArea(){
+        throw new RuntimeException("Not Implemented");
+    }
+
+    public double doubleSize(){
+        throw new RuntimeException("Not Implemented");
+    }
+
+    public double longestLineWithin(){
+        throw new RuntimeException("Not Implemented");
+    }
 }
