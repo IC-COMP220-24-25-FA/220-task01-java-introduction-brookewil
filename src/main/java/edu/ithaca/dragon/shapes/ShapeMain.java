e5dfe25a34eeb91e07806aaf0980697e2c8d2e47
brookewil <bwilson4@ithaca.edu>
2024-09-10 23:13:06 -0400

    Rectangle list that prints area and longestLine

--- src/main/java/edu/ithaca/dragon/shapes/ShapeMain.java
+++ src/main/java/edu/ithaca/dragon/shapes/ShapeMain.java
@@ -1,9 +1,27 @@
 package edu.ithaca.dragon.shapes;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class ShapeMain {
     
     public static void main(String[] args){
         //Make a list of 5 randomly-sized rectangles and print their area and the largest line that can be drawn through them
+
+        List<Rectangle> rectangles = new ArrayList<>();
+        rectangles.add(new Rectangle(3, 5));
+        rectangles.add(new Rectangle(7, 4));
+        rectangles.add(new Rectangle(12.7, 10));
+        rectangles.add(new Rectangle(23, 31.2));
+        rectangles.add(new Rectangle(17.7, 23.9));
+
+        for (int i = 0; i < rectangles.size(); i++){
+            Rectangle element = rectangles.get(i);
+            System.out.println("Rectangle " + (i + 1) + " Area: " + element.calcArea());
+            System.out.println("Rectangle " + (i + 1) + " Longest Line: " + element.longestLineWithin());
+        }
+
         //Allow the user to choose one, double the size of that one, and print them all again
         //Use a loop to repeat the process 5 times
     }
